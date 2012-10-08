2cc5561e71670c651ad3bc3fba3fa6de75023c19
KP Kaiser <kirk@zothcorp.com>
2012-10-08 19:01:26 +0000

    Fixed fabfile for deployment. Still a bit broken though.

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,18 @@
 from __future__ import with_statement
 from fabric.api import *
+from fabric.contrib import files
+
 from contextlib import contextmanager as _contextmanager
 
+# For virtualenv and Fabric
 env.hosts = ['ec2-23-20-147-141.compute-1.amazonaws.com']
 env.user = 'ubuntu'
 env.directory = '/home/ubuntu/shopplyEnv'
 env.activate = 'source /home/ubuntu/shopplyEnv/bin/activate'
+
+# For gunicorn process
+env.remote_workdir = '/home/ubuntu/elasticAPI'
+env.gunicorn_wsgi_app = 'webapp:app'
 
 @_contextmanager
 def virtualenv():
@@ -27,18 +34,26 @@ def prepare_deploy():
     commit()
     push()
 
-def deploy_server():
-    code_dir = '/home/ubuntu/elasticAPI'
+def stop_gunicorn():
     with settings(warn_only=True):
-        if run("nosetests %s" % code_dir).failed:
-            run("git clone git://github.com/burningion/elasticAPI.git %s" % code_dir)
-    with cd(code_dir):
+        if files.exists("%s/gunicorn.pid" % env.remote_workdir):
+            run("kill -9 `cat %s/gunicorn.pid`" % env.remote_workdir)
+
+def restart_gunicorn():
+    stop_gunicorn()
+
+    with virtualenv():
+        run("cd %s && gunicorn -k egg:gunicorn#tornado webapp:app --pid=%s/gunicorn.pid" % (env.remote_workdir, env.remote_workdir))
+
+def deploy_server():
+    with settings(warn_only=True):
+        if run("nosetests %s" % env.remote_workdir).failed:
+            run("git clone git://github.com/burningion/elasticAPI.git %s" % env.remote_workdir)
+    with cd(env.remote_workdir):
         run("git pull")
     with virtualenv():
-        run("pip install -U -r %s/requirements.txt" % code_dir)
-            
-        
-            
-    
+        run("pip install -U -r %s/requirements.txt" % env.remote_workdir)
+    restart_gunicorn()
+
     
 
